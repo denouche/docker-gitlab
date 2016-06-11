762fc5a7e96c115289fa5d1c09e7ab1433b33da3
antoine.leveugle <antoine.leveugle@gmail.com>
2016-06-12 00:19:13 +0200

    fix: mission certificate for TLS postifx configuration, use another

--- postfix-start.sh
+++ postfix-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 postmap /etc/postfix/sasl_passwd
-cat /etc/ssl/certs/Thawte_Premium_Server_CA.pem | tee -a /etc/postfix/cacert.pem
+cat /etc/ssl/certs/ca-certificates.crt | tee -a /etc/postfix/cacert.pem
 /etc/init.d/postfix restart
 
